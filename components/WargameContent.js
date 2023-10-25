d4265af9de770a514fc2b3aa2fff78f6c7e3b81b
Ha_Tan_Luc <hatanluc0@outlook.fr>
2023-10-25 10:09:59 +0200

    modal addinCart

--- components/WargameContent.js
+++ components/WargameContent.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { addToCart } from '../reducers/cart';
 import Sign_up from './Sign_up';
+import Modal from 'react-modal'; // Importez react-modal
 
 function WargameContent(props) {
   
@@ -12,8 +13,13 @@ function WargameContent(props) {
   const cartItems = useSelector((state) => state.cart.cartItems);
   
   const [connected,setConnected]=useState(null)
+  //etat sur la modal
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-
+ 
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   const handleAddToCart = (e) => {
     // Créez un objet représentant les détails du produit
     const product = {
@@ -27,6 +33,7 @@ function WargameContent(props) {
     console.log(product);
     dispatch(addToCart(product));
     // console.log(cartItems)
+    setIsModalOpen(true);
   };
   
   const handleRedirection=(e)=>{
@@ -41,7 +48,7 @@ function WargameContent(props) {
 // if (user.username==null) {
 //   Cart=( <input className={styles.button} type="button" value="Ajouter au panier" onClick={e =>handleRedirection()}></input>)       
 // }else{
-  Cart=( <input className={styles.button} type="button" value="Ajouter au panier" onClick={e => handleAddToCart(e)}></input>)     
+  Cart=( <input className={styles.button} type="button" value="Ajouter au panier" onClick={e => handleAddToCart(e)} ></input>)     
 // }
 
 
@@ -62,6 +69,45 @@ function WargameContent(props) {
           </div>
           {Cart}
         </div>
+        
+          <Modal
+              isOpen={isModalOpen}
+              onRequestClose={closeModal}
+              contentLabel="Ajouter un élément" // Label de contenu pour accessibilité
+              style={{
+                overlay: {
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  position: 'fixed',
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  backgroundColor: 'rgba(255, 255, 255, 0.75)',
+                },
+                content: {
+                  position: 'relative',
+                  top: 'auto',
+                  left: 'auto',
+                  right: 'auto',
+                  bottom: 'auto',
+                  border: '1px solid #ccc',
+                  background: '#fff',
+                  overflow: 'auto',
+                  WebkitOverflowScrolling: 'touch',
+                  borderRadius: '4px',
+                  outline: 'none',
+                  maxWidth: '600px', // Ajustez la largeur selon vos besoins
+                  padding: '20px',
+                },
+              }}
+            >
+              {/* Contenu de la modal */}
+              <h2>Vous avez rajouter ce produit à votre panier</h2>
+              {/* Ajoutez ici le formulaire ou le contenu que vous souhaitez afficher */}
+              <button onClick={closeModal}>Fermer</button>
+          </Modal> 
       </div>
     </>
   );
